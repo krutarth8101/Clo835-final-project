caa2ced635315d062fbebb609cbc582cdad43c55
krutarth-8101 <ec2-user@ip-172-31-27-166.ec2.internal>
2024-04-13 00:34:02 +0000

    updating the portion of code to call tempelates

--- dockerfiles/app.py
+++ dockerfiles/app.py
@@ -60,25 +60,65 @@ def home():
 def about():
     return render_template('about.html', image=image)
 
-# Route for adding employee information
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
-    # Add employee information to the database
-    # Retrieve form data and perform database operations
-    # Add your implementation here
+    emp_id = request.form.get('emp_id')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    primary_skill = request.form.get('primary_skill')
+    location = request.form.get('location')
+
+    if not emp_id or not first_name or not last_name:
+        # If any of the required fields are missing, return an error response
+        return "Error: Employee ID, first name, and last name are required.", 400
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
+        db_conn.commit()
+        emp_name = f"{first_name} {last_name}"  # Use f-string for concatenation
+
+    finally:
+        cursor.close()
+
+    print("All modifications done...")
+    image = download(BUCKETNAME, BGIMG)
     return render_template('addempoutput.html', name=emp_name, image=image)
 
-# Route for getting employee information
+
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
+    image = download(BUCKETNAME, BGIMG)
     return render_template("getemp.html", image=image)
 
-# Route for fetching employee data
+
 @app.route("/fetchdata", methods=['GET','POST'])
 def FetchData():
-    # Fetch employee data from the database
-    # Retrieve form data and perform database query
-    # Add your implementation here
+    emp_id = request.form['emp_id']
+
+    output = {}
+    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (emp_id))
+        result = cursor.fetchone()
+        
+        output["emp_id"] = result[0]
+        output["first_name"] = result[1]
+        output["last_name"] = result[2]
+        output["primary_skills"] = result[3]
+        output["location"] = result[4]
+        
+    except Exception as e:
+        print(e)
+
+    finally:
+        cursor.close()
+
+    image = download(BUCKETNAME, BGIMG)
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
                            lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image=image)
 
